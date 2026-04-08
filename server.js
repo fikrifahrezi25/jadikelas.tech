3f153c6f7160a15e10f46d11c329d0e544f8e096
fikrifahrezi25 <105841106925@student.unismuh.ac.id>
2026-04-08 11:58:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const { ifError } = require("assert");
 const express = require("express");
 const app = express();
-const url = "https://techsprint.exodusai.biz.id";
-
 const PORT = 5526;
-const TARGET_BASE = `${url}/public/index.html`;
+const TARGET_BASE = `/public/index.html`;
 
 app.get("/", (req, res) => {
     const targetUrl = `${TARGET_BASE}`;
@@ -16,5 +14,5 @@ app.get("/public/index.html", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on https://techsprint.exodusai.biz.id/`);
+    console.log(`Server running on http://localhost:5526`);
 });
